57d2f07a91bd625ce4ccad3e2dbed63722d30350
packtleena <leenapatil@packtpub.com>
2018-01-29 14:38:30 +0530

    Delete about.routing.ts

--- Chapter06/Chapter01/stacks/back-button/src/app/pages/about/about.routing.ts
+++
@@ -1,9 +0,0 @@
-import { AboutComponent } from './about.component';
-
-export const AboutRoutes = [
-    { path: 'about', component: AboutComponent },
-];
-
-export const AboutComponents = [
-    AboutComponent
-];
