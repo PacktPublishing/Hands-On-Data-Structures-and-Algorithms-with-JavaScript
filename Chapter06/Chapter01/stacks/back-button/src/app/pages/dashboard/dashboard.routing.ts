2187ff3a14eb5fd69e35b124fbb113dc01dc7da9
packtleena <leenapatil@packtpub.com>
2018-01-29 14:38:51 +0530

    Delete dashboard.routing.ts

--- Chapter06/Chapter01/stacks/back-button/src/app/pages/dashboard/dashboard.routing.ts
+++
@@ -1,9 +0,0 @@
-import { DashboardComponent } from './dashboard.component';
-
-export const DashboardRoutes = [
-    { path: 'dashboard', component: DashboardComponent },
-];
-
-export const DashboardComponents = [
-    DashboardComponent
-];
